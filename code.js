99885c35ae65dfa98b01c9260a3406fa2a900645
torunarea <torunkuet@gmail.com>
2024-11-05 01:36:34 +0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+
+        const timerDiv = document.getElementById('timer');
+
+        const countdown = setInterval(() => {
+            // Calculate minutes and seconds
+            
+           let d=new Date('Dec 19 2024');
+        let sec=(d-new Date())/1000;
+        let days= sec/(24*3600);
+        let rsec= (sec%(24*3600));
+        let hour= rsec/3600;
+            rsec= rsec%3600;
+        let min = rsec/60;
+            rsec= rsec%60;
+            // Display the remaining time
+            timerDiv.innerHTML = 'In <p style="font-size:60px;">'+Math.trunc(days)+' DAYS </p>';
+            timerDiv.innerHTML += Math.trunc(hour)+' HOURS '+Math.trunc(min)+' MINUTES '+Math.trunc(rsec)+' SECONDS';
+
+            // Decrement the countdown time
+            
+        }, 1000);
