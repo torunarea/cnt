9b3071a449d30c883efc4c8edded9a5144e6fbf5
torunarea <torunkuet@gmail.com>
2024-11-09 00:50:24 +0600

    Update code.js

--- code.js
+++ code.js
@@ -55,7 +55,7 @@
                 let love=document.getElementById(id);
                 love.height+=10;
                 love.width+=10;
-                     if(love.height>200)
+                     if(love.height>400)
                 {
                     love.src='dove.png';
                 }
